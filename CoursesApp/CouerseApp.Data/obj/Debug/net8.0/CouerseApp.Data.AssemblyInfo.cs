2a6390d9b69ad8dfa963e618465ae2a6f3e66cf6
Steilor <steilormateo.44@gmail.com>
2024-06-16 19:53:15 -0400

    first commit

--- CoursesApp/CouerseApp.Data/obj/Debug/net8.0/CouerseApp.Data.AssemblyInfo.cs
+++ CoursesApp/CouerseApp.Data/obj/Debug/net8.0/CouerseApp.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CouerseApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d79a1cfb464d15fd36d6d42ae6c796575aae7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b073efa84e6dd4967132995dd561655c5e3c97")]
 [assembly: System.Reflection.AssemblyProductAttribute("CouerseApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CouerseApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
